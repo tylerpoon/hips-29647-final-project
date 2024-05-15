8f74bd09a70419009b60ca204139a290539c20e8
Tyler Poon <tylerpoon@gmail.com>
2024-05-15 13:42:37 -0500

    init commit

---
+++ sunset.glsl
@@ -0,0 +1,29 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+vec3 color_s = vec3(0.5,0.8,0.9);
+
+float plot (vec2 st, float pct){
+  return  smoothstep( pct-0.05, pct, st.y) -
+          smoothstep( pct, pct+0.05, st.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+
+    vec2 sun_coords = vec2(mod(u_time, PI) / PI, abs(sin(u_time)));
+
+    float d = distance(sun_coords, st);
+    float pct = 1.0 - smoothstep(0., 0.1, d);
+
+    vec3 color = vec3(0.0);
+    color = mix(color, color_s, abs(sin(u_time))); 
+    color = mix(color, vec3(0.99,0.72,0.07), pct);
+    gl_FragColor = vec4(color, 1.0);
+}
---
+++ variation_on_a_proof.glsl
@@ -0,0 +1,28 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float plot(vec2 st, float pct){
+  return  smoothstep( pct-0.001, pct, st.y) -
+          smoothstep( pct, pct+0.001, st.y);
+}
+
+//This one needs a lot of work but was good practice
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+
+    float x = (u_time / 10.) + st.x * 3.;
+    float y = pow(x, 3.) - 6. * pow(x, 2.) + 11. * x - 6.;
+    y = (y + 2. - u_time / 5.) / 3.;
+
+    float pct = plot(st,y);
+    float pct_lin = plot(st, ((2. * x - 2.) + 2. - u_time / 5.) / 3.);
+    vec3 color = pct*vec3(0.0,1.0,0.0) + pct_lin*vec3(0.0, 0.0, 1.0);
+
+    gl_FragColor = vec4(color,1.0);
+}
